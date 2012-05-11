c87510e971311eb9fe55b3c24235341c2cca4c99
Chrisharris71 <chris.harris71@yahoo.com>
2012-05-10 20:33:20 -0400

    Fixed DB

--- db/schema.rb
+++ db/schema.rb
@@ -16,13 +16,14 @@ ActiveRecord::Schema.define(:version => 20120510010956) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "password_digest"
     t.string   "remember_token"
     t.boolean  "admin",           :default => false
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
