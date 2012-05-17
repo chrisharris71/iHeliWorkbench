922967ddeb0d2e595ec6140594affa81e222c24e
Chrisharris71 <chris.harris71@yahoo.com>
2012-05-17 17:36:11 -0400

    Fixed association problems

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,24 +1,30 @@
 class Project < ActiveRecord::Base
-  attr_accessible :cyclic_servo_id, :elec_motor_id, :engine_id, :exhaust_id, :fbl_unit_id, :governor_id, :heli_kit_id, :main_blade_id, :paddle_id, :receiver_id, :rudder_gyro_id, :rudder_servo_id, :speed_control_id, :tail_blade_id, :throttle_servo_id, :user_id, :electric, :flybarless, :heli_kit_attributes 
-
+  attr_accessible :cyclic_servo_id, :elec_motor_id, :engine_id, :exhaust_id, :fbl_unit_id, :governor_id, :heli_kit_id, :main_blade_id, :paddle_id, :receiver_id, :rudder_gyro_id, :rudder_servo_id, :speed_control_id, :tail_blade_id, :throttle_servo_id, :user_id, :electric, :flybarless
+	
 	belongs_to :user
 #   <-- All the components are listed here -->
-  has_one :engine
-  has_one :main_blade
-  has_one :heli_kit
-  has_one :exhaust
-  has_one :elec_motor
-  has_one :speed_control
-  has_one :cyclic_servo
-  has_one :rudder_servo
-  has_one :throttle_servo
-  has_one :rudder_gyro
-  has_one :fbl_unit
-  has_one :governor
-  has_one :tail_blade
-  has_one :paddle
-  has_one :receiver
+  belongs_to :engine
+  belongs_to :main_blade
+  belongs_to :heli_kit
+  belongs_to :exhaust
+  belongs_to :elec_motor
+  belongs_to :speed_control
+  belongs_to :cyclic_servo
+  belongs_to :rudder_servo
+  belongs_to :throttle_servo
+  belongs_to :rudder_gyro
+  belongs_to :fbl_unit
+  belongs_to :governor
+  belongs_to :tail_blade
+  belongs_to :paddle
+  belongs_to :receiver
  	
- 	accepts_nested_attributes_for :heli_kit
+ 	def heli_kit_name
+ 		if heli_kit
+ 			heli_kit.name
+ 			else
+ 			""
+ 		end
+ 	end
  	
 end
