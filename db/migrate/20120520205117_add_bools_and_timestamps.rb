37fa2647eab4a4bca5c71c97faf1e16bad23dc16
Christopher <chris.harris71@yahoo.com>
2012-05-28 11:10:31 -0400

    migrations

--- db/migrate/20120520205117_add_bools_and_timestamps.rb
+++ db/migrate/20120520205117_add_bools_and_timestamps.rb
@@ -2,42 +2,42 @@ class AddBoolsAndTimestamps < ActiveRecord::Migration
   def up
   
 #   add_timestamps(:users)
-  add_timestamps(:heli_kits)
-  add_timestamps(:engines)
-  add_timestamps(:fbl_units)
-  add_timestamps(:exhausts)
-  add_timestamps(:receivers)
-  add_timestamps(:rudder_servos)
-  add_timestamps(:throttle_servos)
-  add_timestamps(:cyclic_servos)
-  add_timestamps(:rudder_gyros)
-  add_timestamps(:elec_motors)
-  add_timestamps(:speed_controls)
-  add_timestamps(:governors)
-  add_timestamps(:main_blades)
-  add_timestamps(:tail_blades)
-  add_timestamps(:paddles)
-  add_timestamps(:manufacturers)
-  add_timestamps(:admins)
+  # add_timestamps(:heli_kits)
+#   add_timestamps(:engines)
+#   add_timestamps(:fbl_units)
+#   add_timestamps(:exhausts)
+#   add_timestamps(:receivers)
+#   add_timestamps(:rudder_servos)
+#   add_timestamps(:throttle_servos)
+#   add_timestamps(:cyclic_servos)
+#   add_timestamps(:rudder_gyros)
+#   add_timestamps(:elec_motors)
+#   add_timestamps(:speed_controls)
+#   add_timestamps(:governors)
+#   add_timestamps(:main_blades)
+#   add_timestamps(:tail_blades)
+#   add_timestamps(:paddles)
+#   add_timestamps(:manufacturers)
+#   add_timestamps(:admins)
 #   add_timestamps(:projects)
-  add_timestamps(:microposts)
-  add_timestamps(:admin_users)
-  add_timestamps(:active_admin_comments)
-   						
-  add_column :admin_users, :current_sign_in_at, :timestamp
-  add_column :admin_users, :last_sign_in_at, :timestamp
-  add_column :admin_users, :reset_password_sent_at, :timestamp
-  add_column :admin_users, :remember_created_at, :timestamp
+  # add_timestamps(:microposts)
+#   add_timestamps(:admin_users)
+#   add_timestamps(:active_admin_comments)
+#    						
+#   add_column :admin_users, :current_sign_in_at, :timestamp
+#   add_column :admin_users, :last_sign_in_at, :timestamp
+#   add_column :admin_users, :reset_password_sent_at, :timestamp
+#   add_column :admin_users, :remember_created_at, :timestamp
 
   
   
 #   add_column :projects, :name, :string
-  add_column :users, :admin, :boolean, default: false
+#   add_column :users, :admin, :boolean, default: false
 #   add_column :projects, :electric, :boolean
 #   add_column :projects, :flybarless, :boolean
-  add_column :receivers, :has_gov, :boolean
-  add_column :receivers, :has_volt_reg, :boolean
-  add_column :receivers, :has_fbl_unit, :boolean
+  # add_column :receivers, :has_gov, :boolean
+#   add_column :receivers, :has_volt_reg, :boolean
+#   add_column :receivers, :has_fbl_unit, :boolean
   
   # add_timestamps(:current_sign_in_at)
 #   add_timestamps(:last_sign_in_at)
